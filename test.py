61506db1d3b8750e47c9fcf835360a798d7219fc
Anagha630 <anagha630@gmail.com>
2023-11-11 13:27:47 -0500

    Exception handling added and few tests modified

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ class TestMETRICSTICS(unittest.TestCase):
         self.assertEqual(self.metrics.mode(), [1, 2, 3, 4, 5])
 
     def test_standard_deviation(self):
-        self.assertAlmostEqual(self.metrics.standard_deviation(), 1.5811, places=4)
+        self.assertAlmostEqual(self.metrics.standard_deviation(), 1.4142, places=4)
 
     def test_mad(self):
         self.assertEqual(self.metrics.mad(), 1.2)
@@ -44,56 +44,57 @@ class TestMETRICSTICS(unittest.TestCase):
         self.assertEqual(metrics.mad(), 0)
         self.assertEqual(metrics.min_max(), (5, 5))
 
+    def test_even_number_data_point(self):
+        metrics = METRICSTICS([1, 2, 3, 4, 5, 6])
+        self.assertEqual(metrics.mean(), 3.5)
+        self.assertEqual(metrics.median(), 3.5)
+        self.assertEqual(metrics.mode(), [1, 2, 3, 4, 5, 6])
+        self.assertAlmostEqual(metrics.standard_deviation(), 1.7078, places=4)
+        self.assertEqual(metrics.mad(), 1.5)
+        self.assertEqual(metrics.min_max(), (1, 6))
+
     def test_negative_numbers(self):
         metrics = METRICSTICS([-1, -2, -3, -4, -5])
         self.assertEqual(metrics.mean(), -3.0)
         self.assertEqual(metrics.median(), -3)
         self.assertEqual(metrics.mode(), [-5, -4, -3, -2, -1])
-        self.assertAlmostEqual(metrics.standard_deviation(), 1.5811, places=4)
+        self.assertAlmostEqual(metrics.standard_deviation(), 1.4142, places=4)
         self.assertEqual(metrics.mad(), 1.2)
         self.assertEqual(metrics.min_max(), (-5, -1))
 
-    def test_floating_point_numbers(self):
-        metrics = METRICSTICS([1.5, 2.5, 3.5])
-        self.assertAlmostEqual(metrics.mean(), 2.5, places=4)
-        self.assertAlmostEqual(metrics.median(), 2.5, places=4)
-        self.assertEqual(metrics.mode(), [1.5, 2.5, 3.5])
-        self.assertAlmostEqual(metrics.standard_deviation(), 0.8165, places=4)
-        self.assertAlmostEqual(metrics.mad(), 0.6667, places=4)
-        self.assertEqual(metrics.min_max(), (1.5, 3.5))
-
     def test_duplicate_values(self):
         metrics = METRICSTICS([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5])
-        self.assertAlmostEqual(metrics.mean(), 3.0)
-        self.assertEqual(metrics.median(), 3)
-        self.assertEqual(metrics.mode(), [3, 4, 5])
-        self.assertAlmostEqual(metrics.standard_deviation(), 1.0, places=4)
-        self.assertAlmostEqual(metrics.mad(), 0.8, places=4)
+        self.assertAlmostEqual(metrics.mean(), 3.6667, places=4)
+        self.assertEqual(metrics.median(), 4)
+        self.assertEqual(metrics.mode(), [5])
+        self.assertAlmostEqual(metrics.standard_deviation(), 1.2472, places=4)
+        self.assertAlmostEqual(metrics.mad(), 1.0667, places=4)
         self.assertEqual(metrics.min_max(), (1, 5))
 
     def test_large_data_set(self):
         data = list(range(1, 10001))
         metrics = METRICSTICS(data)
         self.assertEqual(metrics.mean(), 5000.5)
-        self.assertEqual(metrics.median(), 5000)
-        self.assertEqual(metrics.mode(), [1])
-        self.assertAlmostEqual(metrics.standard_deviation(), 2886.8956, places=4)
-        self.assertAlmostEqual(metrics.mad(), 2886.5, places=4)
+        self.assertEqual(metrics.median(), 5000.5)
+        self.assertEqual(metrics.mode(), list(range(1,10001)))
+        self.assertAlmostEqual(metrics.standard_deviation(), 2886.7513, places=4)
+        self.assertAlmostEqual(metrics.mad(), 2500, places=4)
         self.assertEqual(metrics.min_max(), (1, 10000))
 
     def test_mixed_data_types(self):
-        metrics = METRICSTICS([1, 2, 3, 4, "5"])
         with self.assertRaises(TypeError):
+            metrics = METRICSTICS([1, 2, 3, 4, "5"])
+        with self.assertRaises(UnboundLocalError):
             metrics.mean()
-        with self.assertRaises(TypeError):
+        with self.assertRaises(UnboundLocalError):
             metrics.median()
-        with self.assertRaises(TypeError):
+        with self.assertRaises(UnboundLocalError):
             metrics.mode()
-        with self.assertRaises(TypeError):
+        with self.assertRaises(UnboundLocalError):
             metrics.standard_deviation()
-        with self.assertRaises(TypeError):
+        with self.assertRaises(UnboundLocalError):
             metrics.mad()
-        with self.assertRaises(TypeError):
+        with self.assertRaises(UnboundLocalError):
             metrics.min_max()
 
 if __name__ == '__main__':
